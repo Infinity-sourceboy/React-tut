fc980225912d0d4f337dcf4adb42eab2d30f30f5
Infinity-sourceboy <97177843+Infinity-sourceboy@users.noreply.github.com>
2025-04-20 23:23:37 +0530

    submit
    submit

---
+++ react pratices/src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.css'
+import Gallery from './component.jsx' 
+
+function App() {
+  let name = "raj"
+  return (
+    <>
+      <h2>my name is {name}</h2>
+      <Gallery/>
+    </>
+  )
+}
+ 
+
+export default App
---
+++ react pratices/src/component.jsx
@@ -0,0 +1,8 @@
+
+export default Gallery(){
+    return (
+        <div>
+        <h2>rajesh & arpita</h2>
+        </div>
+    )
+}
